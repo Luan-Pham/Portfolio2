b0b3239b28428435de6396bc51131ed845d655db
Luan Pham <luanpham@Luans-MacBook-Pro.local>
2022-12-23 13:47:17 -0800

    added about template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import * as React from 'react';
-import { ChakraProvider, VStack } from '@chakra-ui/react';
-import Header from './components/Header';
-// import Project from './components/Project';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import * as React from "react";
+import { ChakraProvider, VStack } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <ChakraProvider>
       <VStack p={8}>
         <Header />
+        <Profile />
         <Projects />
         <Skills />
       </VStack>
